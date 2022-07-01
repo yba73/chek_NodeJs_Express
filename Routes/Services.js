0e15ed5c842fddbcff96e1c316e0f347b432d085
origin <yba73mos@gmail.com>
2022-07-01 03:10:26 +0100

    services

--- Routes/Services.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-module.exports = router;
-
-const path = require("path");
-router.get("/services", (req, res) =>
-  res.sendFile(path.join(__dirname, "../", "views", "Our Services.html"))
-);
